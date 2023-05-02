0f60063df8b3388ebb8ca6ba672408f56345e6d5
Carlos <carlosantonio.pereiradasilva@gmail.com>
2023-05-02 18:18:05 -0300

    Desafio ContaBanco foi concluido e testado.

--- TrilhaJavaBasico/ContaBanco/src/ContaTerminal.java
+++ TrilhaJavaBasico/ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+import java.util.Locale;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+          
+        System.out.println("Por favor, digite o número da Conta:");
+        int numero = sc.nextInt();
+        sc.nextLine();
+
+        System.out.println("Agora digite o número da Agência:");
+        String agencia = sc.nextLine();
+        
+        System.out.println("Por favor, digite o seu nome:");
+        String nomeDoCliente = sc.nextLine();
+        
+        System.out.println("Por favor digite o valor a ser depositado:");
+        double saldo = sc.nextDouble();
+
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo de %.2f já está disponível para saque.", nomeDoCliente, agencia, numero, saldo);
+
+        sc.close();
     }
 }
